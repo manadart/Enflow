f67ce6f49833e599a253f3b5127a9666da98d488
Joseph Phillips <jphillips.pro@gmail.com>
2013-04-09 17:27:20 +1000

    Tests for expressions and filtering.

--- src/Enflow.Base.Test/StateRuleTests.cs
+++ src/Enflow.Base.Test/StateRuleTests.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace Enflow.Base.Test
 {
     public class PositiveCounterRule : StateRule<CounterModel>
     {
-        public override bool IsSatisfied(CounterModel candidate) { return candidate.Counter > 0; }
+        public override Expression<Func<CounterModel, bool>> Predicate { get { return candidate => candidate.Counter > 0; } }
     }
 
     public class NegativeCounterRule : StateRule<CounterModel>
     {
-        public override bool IsSatisfied(CounterModel candidate) { return candidate.Counter < 0; }
+        public override Expression<Func<CounterModel, bool>> Predicate { get { return candidate => candidate.Counter < 0; } }
+    }
+
+    public class LessThanTenCounterRule : StateRule<CounterModel>
+    {
+        public override Expression<Func<CounterModel, bool>> Predicate { get { return candidate => candidate.Counter < 10; } }
     }
 
     public class StateRuleTests
@@ -22,14 +29,6 @@ namespace Enflow.Base.Test
             var model = new CounterModel();
             model.Increment();
             Assert.True(new PositiveCounterRule().IsSatisfied(model));
-        }
-
-        [Fact]
-        public void NotExtensionEvaluesCorrectly()
-        {
-            var model = new CounterModel();
-            model.Increment();
-            Assert.False(new PositiveCounterRule().Not().IsSatisfied(model));
         }
 
         [Fact]
@@ -56,13 +55,60 @@ namespace Enflow.Base.Test
         }
 
         [Fact]
-        public void AsExpressionExtensionFiltersCorrectly()
+        public void NotExtensionEvaluesCorrectly()
+        {
+            var model = new CounterModel();
+            model.Increment();
+            Assert.False(new PositiveCounterRule().Not().IsSatisfied(model));
+        }
+
+        [Fact]
+        public void PredicateFiltersCorrectly()
         {
             var validCandidate = new CounterModel { Counter = 1 };
             var invalidCandidate = new CounterModel { Counter = -1 };
 
             var candidates = new List<CounterModel> { validCandidate, invalidCandidate }.AsQueryable();
             var filtered = candidates.Where(new PositiveCounterRule().Predicate).ToList();
+
+            Assert.Equal(1, filtered.Count);
+            Assert.Equal(validCandidate.Id, filtered[0].Id);
+        }
+
+        [Fact]
+        public void AndPredicateFiltersCorrectly()
+        {
+            var validCandidate = new CounterModel { Counter = 1 };
+            var invalidCandidate = new CounterModel { Counter = -1 };
+
+            var candidates = new List<CounterModel> { validCandidate, invalidCandidate }.AsQueryable();
+            var filtered = candidates.Where(new PositiveCounterRule().And(new LessThanTenCounterRule()).Predicate).ToList();
+
+            Assert.Equal(1, filtered.Count);
+            Assert.Equal(validCandidate.Id, filtered[0].Id);
+        }
+
+        [Fact]
+        public void OrPredicateFiltersCorrectly()
+        {
+            var validCandidate = new CounterModel { Counter = 1 };
+            var invalidCandidate = new CounterModel { Counter = 20 };
+
+            var candidates = new List<CounterModel> { validCandidate, invalidCandidate }.AsQueryable();
+            var filtered = candidates.Where(new NegativeCounterRule().Or(new LessThanTenCounterRule()).Predicate).ToList();
+
+            Assert.Equal(1, filtered.Count);
+            Assert.Equal(validCandidate.Id, filtered[0].Id);
+        }
+
+        [Fact]
+        public void NotPredicateFiltersCorrectly()
+        {
+            var validCandidate = new CounterModel { Counter = 1 };
+            var invalidCandidate = new CounterModel { Counter = -1 };
+
+            var candidates = new List<CounterModel> { validCandidate, invalidCandidate }.AsQueryable();
+            var filtered = candidates.Where(new NegativeCounterRule().Not().Predicate).ToList();
 
             Assert.Equal(1, filtered.Count);
             Assert.Equal(validCandidate.Id, filtered[0].Id);
--- src/Enflow.Base.Test/TestingImplementations.cs
+++ src/Enflow.Base.Test/TestingImplementations.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 namespace Enflow.Base.Test
 {
@@ -17,13 +16,5 @@ namespace Enflow.Base.Test
         public CounterIncrementWorkflow() { }
         public CounterIncrementWorkflow(IStateRule<CounterModel> preRule) : base(preRule) { }
         protected override void ExecuteWorkflow(CounterModel candidate) { candidate.Increment(); }
-    }
-
-    public class LessThanTen : StateRule<CounterModel>
-    {
-        public override bool IsSatisfied(CounterModel candidate)
-        {
-            return candidate.Counter < 10;
-        }
     }
 }
