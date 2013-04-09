39bf974a5173462341760e9eb4df95a18cc4c35b
Joseph Phillips <jphillips.pro@gmail.com>
2013-04-09 17:26:06 +1000

    Added Parameter replacer for expressions.

---
+++ src/Enflow.Base/ParameterReplacer.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Enflow.Base
+{
+    public class ParameterReplacer : ExpressionVisitor
+    {
+        private readonly ParameterExpression _parameter;
+
+        public ParameterReplacer(ParameterExpression parameter) { _parameter = parameter; }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return base.VisitParameter(_parameter);
+        }
+    }
+}
